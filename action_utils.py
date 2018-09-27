4653da21477b6d706826a3aa3e9c56d9e3af0e5a
Andeep Toor <andeeptoor@andeeps-mbp.wma.ibm.com>
2018-09-27 11:25:04 -0400

    Initial check-in of table-based RL model

---
+++ action_utils.py
@@ -0,0 +1,239 @@
+from pysc2.lib import actions, features, units
+from enum import Enum, auto
+import random
+
+valid_actions = [
+	'NOTHING',
+	'SELECT_SCV',
+	'TRAIN_BARRACKS_MARINE',
+	'TRAIN_SCV',
+	'TRAIN_BARRACKS_REAPER',
+	'TRAIN_FACTORY_HELLION',
+	'TRAIN_FACTORY_TANK',
+	'TRAIN_BARRACKS_MARAUDER',
+	'SELECT_BARRACKS',
+	'SELECT_COMMAND_CENTER',
+	'SELECT_FACTORY',
+	'BUILD_BARRACKS',
+	'BUILD_BARRACKS_TECH_LAB',
+	'SELECT_ENG_BAY',
+	'BUILD_SUPPLY_DEPOT',
+	'BUILD_COMMAND_CENTER',
+	'BUILD_REFINERY',
+	'BUILD_FACTORY',
+	'BUILD_FACTORY_TECH_LAB',
+	'BUILD_ENG_BAY',
+	'SELECT_ARMY',
+	'RESEARCH_INFANTRY_WEAPONS_UPGRADE',
+	'LAND',
+]
+
+for x in range(64):
+	for y in range(64):
+		# valid_actions.append('MOVE_%d_%d' % (x, y))
+		if (x + 1) % 16 == 0 and (y + 1) % 16 == 0:
+			valid_actions.append('ATTACK_%d_%d' % (x - 8, y - 8))
+
+class ActionUtils:
+
+	def count_units(self, obs, unit_type):
+		return len(self.get_units(obs, unit_type))
+
+	def get_units(self, obs, unit_type):
+		return [unit for unit in obs.observation.feature_units if unit.unit_type == unit_type]
+
+	def unit_type_is_selected(self, obs, unit_type):
+		if (len(obs.observation.single_select) > 0 and obs.observation.single_select[0].unit_type == unit_type):
+		  return True
+		
+		if (len(obs.observation.multi_select) > 0 and obs.observation.multi_select[0].unit_type == unit_type):
+		  return True
+		
+		return False
+
+	def can_do(self, action, obs):
+		return action in obs.observation.available_actions
+
+	def nothing(self):
+		return actions.FUNCTIONS.no_op()
+
+	def near(self, unit):
+		x = ((-1)**random.randrange(2) * random.randint(0,30)) + unit.x
+		y = ((-1)**random.randrange(2) * random.randint(0,30)) + unit.y
+		return x,y
+
+	def anywhere(self):
+		return (random.randint(0, 83),random.randint(0, 83))
+
+	def train(self, production_unit, action, obs):
+		if self.unit_type_is_selected(obs, production_unit) and self.can_do(action.id, obs):
+			return action("queued")
+		else:
+			return self.nothing()
+
+	def build(self, production_unit, action, obs, loc=None):
+		if loc is None:
+			loc = self.anywhere()
+		if self.unit_type_is_selected(obs, production_unit) and self.can_do(action.id, obs):
+			return action("queued", loc)
+		else:
+			return self.nothing()
+
+	def select(self,type, obs, all=True):
+		group = self.get_units(obs, type)
+		group_unit = self.find_valid(group)
+		if group_unit is not None:
+			if all:
+				action_name = "select_all_type"
+			else:
+				action_name = "select"
+			return actions.FUNCTIONS.select_point(action_name, (group_unit.x, group_unit.y))
+		else:
+			return self.nothing()
+
+	def find_valid(self, choices):
+		valid_choices = self.find_all_valid(choices)
+		if len(valid_choices) > 0:
+			return valid_choices[0]
+		else:
+			return None
+
+	def find_all_valid(self, choices):
+		return [s for s in choices if s.x >= 0 and s.y >= 0 and s.x < 84 and s.y < 84]
+
+	def transform_distance(self, x, x_distance, y, y_distance, base_top_left):
+		if not base_top_left:
+			return [x - x_distance, y - y_distance]
+		
+		return [x + x_distance, y + y_distance]
+
+	def transform_location(self, x, y, base_top_left):
+		if not base_top_left:
+			return [64 - x, 64 - y]
+		
+		return [x, y]
+
+	def do(self, obs, choice, base_top_left, base_loc):
+		# if obs.first():
+		# 	player_y, player_x = (obs.observation.feature_minimap.player_relative == features.PlayerRelative.SELF).nonzero()
+		# 	xmean = player_x.mean()
+		# 	ymean = player_y.mean()
+
+		# 	if xmean <= 31 and ymean <= 31:
+		# 		self.attack_coordinates = (49, 49)
+		# 	else:
+		# 		self.attack_coordinates = (12, 16)
+
+		print(choice)
+
+		if choice == 'NOTHING':
+			return self.nothing()
+		elif choice.startswith('ATTACK_'):
+			attack_elements = choice.split('_')
+			#SCV Hack!
+			# if self.can_do(actions.FUNCTIONS.Attack_minimap.id, obs):
+			if self.can_do(actions.FUNCTIONS.Attack_minimap.id, obs) and not self.unit_type_is_selected(obs, units.Terran.SCV):
+
+				x_offset = random.randint(-1, 1)
+				y_offset = random.randint(-1, 1)
+
+				x = int(attack_elements[1]) + (x_offset * 8)
+				y = int(attack_elements[2]) + (y_offset * 8)
+
+				return actions.FUNCTIONS.Attack_minimap("now", self.transform_location(x,y, base_top_left))
+			else:
+				return self.nothing()
+		elif choice == 'SELECT_SCV':
+			if obs.observation.player.idle_worker_count > 0:
+				return actions.FUNCTIONS.select_idle_worker("select")
+			return self.select(units.Terran.SCV, obs, all=False)
+		elif choice == "LAND":
+			if self.can_do(actions.FUNCTIONS.Land_screen.id, obs):
+				return actions.FUNCTIONS.Land_screen("queued", self.anywhere())
+			else:
+				return self.nothing()
+		elif choice == 'BUILD_COMMAND_CENTER':
+			return self.build(units.Terran.SCV, actions.FUNCTIONS.Build_CommandCenter_screen, obs)
+		elif choice == 'BUILD_FACTORY':
+			return self.build(units.Terran.SCV, actions.FUNCTIONS.Build_Factory_screen, obs)
+		elif choice == 'BUILD_FACTORY_TECH_LAB':
+			return self.build(units.Terran.Factory, actions.FUNCTIONS.Build_TechLab_screen, obs)
+		elif choice == 'BUILD_ENG_BAY':
+			return self.build(units.Terran.SCV, actions.FUNCTIONS.Build_EngineeringBay_screen, obs)
+		elif choice == 'BUILD_BARRACKS':
+			return self.build(units.Terran.SCV, actions.FUNCTIONS.Build_Barracks_screen, obs)
+		elif choice == 'BUILD_SUPPLY_DEPOT':
+			return self.build(units.Terran.SCV, actions.FUNCTIONS.Build_SupplyDepot_screen, obs)
+		elif choice == 'BUILD_BARRACKS_TECH_LAB':
+			return self.build(units.Terran.Barracks, actions.FUNCTIONS.Build_TechLab_screen, obs)
+		elif choice == 'SELECT_BARRACKS':
+			return self.select(units.Terran.Barracks, obs)
+		elif choice == 'SELECT_COMMAND_CENTER':
+			return self.select(units.Terran.CommandCenter, obs)
+		elif choice == 'SELECT_FACTORY':
+			return self.select(units.Terran.Factory, obs)
+		elif choice == 'SELECT_ENG_BAY':
+			return self.select(units.Terran.EngineeringBay, obs)
+		elif choice == 'TRAIN_FACTORY_HELLION':
+			return self.train(units.Terran.Factory, actions.FUNCTIONS.Train_Hellion_quick, obs)
+		elif choice == 'TRAIN_FACTORY_TANK':
+			return self.train(units.Terran.Factory, actions.FUNCTIONS.Train_SiegeTank_quick, obs)
+		elif choice == 'TRAIN_BARRACKS_MARINE':
+			return self.train(units.Terran.Barracks, actions.FUNCTIONS.Train_Marine_quick, obs)
+		elif choice == 'TRAIN_BARRACKS_REAPER':
+			return self.train(units.Terran.Barracks, actions.FUNCTIONS.Train_Reaper_quick, obs)
+		elif choice == 'TRAIN_BARRACKS_MARAUDER':
+			return self.train(units.Terran.Barracks, actions.FUNCTIONS.Train_Marauder_quick, obs)
+		elif choice == 'TRAIN_SCV':
+			return self.train(units.Terran.CommandCenter, actions.FUNCTIONS.Train_SCV_quick, obs)
+		elif choice == 'RESEARCH_INFANTRY_WEAPONS_UPGRADE':
+			return self.train(units.Terran.EngineeringBay, actions.FUNCTIONS.Research_TerranInfantryWeapons_quick, obs)
+		elif choice == 'BUILD_REFINERY':
+			geysers = self.find_all_valid(self.get_units(obs, units.Neutral.VespeneGeyser))
+			extractors = self.find_all_valid(self.get_units(obs, units.Terran.Refinery))
+			extractor_locs = set([(e.x, e.y) for e in extractors])
+
+			open_geysers = [g for g in geysers if (g.x, g.y) not in extractor_locs]
+			if len(open_geysers) > 0:
+				return self.build(units.Terran.SCV, actions.FUNCTIONS.Build_Refinery_screen, obs, (open_geysers[0].x, open_geysers[0].y))
+			else:
+				return self.nothing()
+			# if self.unit_type_is_selected(obs, units.Terran.SCV) and self.can_do(actions.FUNCTIONS.Build_Refinery_screen.id, obs):
+			# 	geysers = self.get_units(obs, units.Neutral.VespeneGeyser)
+			# 	if len(geysers) > 0:
+			# 		geyser = random.choice(geysers)
+			# 		return actions.FUNCTIONS.Build_Refinery_screen("now", (geyser.x, geyser.y))
+			# 	else:
+			# 		return self.nothing(obs)
+			# else:
+			# 	return self.nothing(obs)
+		# elif choice == RETURN_SCV_MINING:
+		# 	minerals = self.get_units(obs, units.Neutral.MineralField)
+		# 	if len(minerals) > 0 and self.unit_type_is_selected(obs, units.Terran.SCV) and self.can_do(actions.FUNCTIONS.Harvest_Gather_screen.id, obs):
+		# 		mineral = random.choice(minerals)
+		# 		return actions.FUNCTIONS.Harvest_Gather_screen("now", (mineral.x, mineral.y))
+		# 	else:
+		# 		return self.nothing(obs)
+		elif choice == 'SELECT_ARMY':
+			if self.can_do(actions.FUNCTIONS.select_army.id, obs):
+				return actions.FUNCTIONS.select_army("select")
+			else:
+				return self.nothing()
+		# elif choice == PATROL_ARMY:
+		# 	if self.unit_type_is_selected(obs, units.Terran.Marine) and self.can_do(actions.FUNCTIONS.Move_screen.id, obs):
+		# 		command_centers = self.get_units(obs, units.Terran.CommandCenter)
+		# 		if len(command_centers) > 0:
+		# 			command_center = random.choice(command_centers)
+		# 			loc = self.near(command_center)
+		# 		else:
+		# 			loc = (random.randint(0, 83),random.randint(0, 83))
+		# 		return actions.FUNCTIONS.Move_screen("now",loc)
+		# 	else:
+		# 		return self.nothing(obs)
+		# elif choice == ATTACK:
+		# 	if self.can_do(actions.FUNCTIONS.Attack_minimap.id, obs):
+		# 		return actions.FUNCTIONS.Attack_minimap("now", self.attack_coordinates)
+		# 	else:
+		# 		return self.nothing(obs)
+		else:
+			return self.nothing()
---
+++ table_q_learn_agent.py
@@ -0,0 +1,233 @@
+
+import random
+from action_utils import valid_actions, ActionUtils
+from qlearn import QLearningTable
+from pysc2.agents import base_agent
+from pysc2.lib import actions, features, units
+import numpy as np
+import math
+import os
+import pandas as pd
+import datetime
+from collections import deque
+
+_PLAYER_HOSTILE = 4
+_PLAYER_SELF = 1
+
+action_mapping = {
+	'NOTHING':['NOTHING'],
+	'TRAIN_BARRACKS_MARINE':['SELECT_BARRACKS','TRAIN_BARRACKS_MARINE'],
+	'TRAIN_BARRACKS_REAPER':['SELECT_BARRACKS','TRAIN_BARRACKS_REAPER'],
+	'TRAIN_BARRACKS_MARAUDER':['SELECT_BARRACKS','TRAIN_BARRACKS_MARAUDER'],
+	'TRAIN_FACTORY_HELLION':['SELECT_FACTORY','TRAIN_FACTORY_HELLION'],
+	'TRAIN_FACTORY_TANK':['SELECT_FACTORY','TRAIN_FACTORY_TANK'],
+	# 'TRAIN_SCV':['SELECT_COMMAND_CENTER','TRAIN_SCV'],
+	'BUILD_COMMAND_CENTER':['SELECT_SCV','BUILD_COMMAND_CENTER'],
+	'BUILD_BARRACKS':['SELECT_SCV','BUILD_BARRACKS'],
+	'BUILD_SUPPLY_DEPOT':['SELECT_SCV','BUILD_SUPPLY_DEPOT'],
+	'BUILD_REFINERY':['SELECT_SCV','BUILD_REFINERY'],
+	'BUILD_ENG_BAY':['SELECT_SCV','BUILD_ENG_BAY'],
+	'BUILD_FACTORY':['SELECT_SCV','BUILD_FACTORY'],
+	'BUILD_FACTORY_TECH_LAB':['SELECT_FACTORY','BUILD_FACTORY_TECH_LAB','LAND','LAND'],
+	'BUILD_BARRACKS_TECH_LAB':['SELECT_BARRACKS','BUILD_BARRACKS_TECH_LAB','LAND','LAND']
+}
+
+for mm_x in range(0, 64):
+	for mm_y in range(0, 64):
+		if (mm_x + 1) % 32 == 0 and (mm_y + 1) % 32 == 0:
+			attack = 'ATTACK_' + str(mm_x - 16) + '_' + str(mm_y - 16)
+			action_mapping[attack] = ['SELECT_ARMY', attack]
+
+high_actions = list(action_mapping.keys())
+
+class TableQLearnAgent(base_agent.BaseAgent):
+
+	def __init__(self):
+		super(TableQLearnAgent, self).__init__()
+		self.utils = ActionUtils()
+		self.qlearn = QLearningTable(actions=list(range(len(high_actions))))
+		self.output_file = 'model/table_q_learn_agent.csv'
+		self.output_actions_prefix = 'games/game_actions_%d.csv'
+		self.output_game_outcome_file = 'game_state/game_outcome.csv'
+		
+		if os.path.exists(self.output_game_outcome_file):
+			self.game_outcomes = pd.read_csv(self.output_game_outcome_file).to_dict('records')
+		else:
+			self.game_outcomes = []
+
+		self.game_count = len([f for f in os.listdir('games') if os.path.splitext(f)[1] == ".csv"])
+		if os.path.isfile(self.output_file + '.gz'):
+			self.qlearn.q_table = pd.read_pickle(self.output_file + '.gz', compression='gzip')
+
+	def reset(self):
+		super(TableQLearnAgent, self).reset()
+		self.previous_action = None
+		self.previous_state = None
+		self.queue = deque()
+		self.game_actions = []
+		self.game_count += 1
+		print('Game %d' % (self.game_count))
+
+	def create_state(self, obs):
+		command_center_count = len(self.utils.get_units(obs, units.Terran.CommandCenter))
+		barracks_count = len(self.utils.get_units(obs, units.Terran.Barracks))
+		barracks_tech_lab_count = self.utils.count_units(obs, units.Terran.BarracksTechLab)
+		supply_depot_count = len(self.utils.get_units(obs, units.Terran.SupplyDepot))
+		refinery_count = len(self.utils.get_units(obs, units.Terran.Refinery))
+		eng_bay_count = len(self.utils.get_units(obs, units.Terran.EngineeringBay))
+		factory_count = len(self.utils.get_units(obs, units.Terran.Factory))
+		factory_tech_lab_count = len(self.utils.get_units(obs, units.Terran.FactoryTechLab))
+		army_supply = obs.observation.player.food_army
+		worker_supply = obs.observation.player.food_workers
+		supply_free = obs.observation.player.food_cap - obs.observation.player.food_used
+		vespene = obs.observation.player.vespene
+
+		state_elements = 8
+		current_state = np.zeros(8 + 4 + 4)
+		current_state[0] = command_center_count
+		current_state[1] = supply_depot_count
+		current_state[2] = barracks_count
+		current_state[3] = refinery_count
+		current_state[4] = eng_bay_count
+		current_state[5] = factory_count
+		current_state[6] = army_supply
+		current_state[7] = worker_supply
+
+		# current_state[3] = len(self.utils.get_units(obs, units.Terran.EngineeringBay))
+		# current_state[4] = len(self.utils.get_units(obs, units.Terran.Factory))
+		# current_state[2] = len(self.utils.get_units(obs, units.Terran.SCV))
+		# current_state[3] = obs.observation.player.food_cap
+		# current_state[6] = obs.observation.player.food_used / (obs.observation.player.food_cap + 0.01)
+		# current_state[4] = obs.observation.player.army_count
+		# current_state[0] = self.quantize(obs.observation.player.minerals)
+		# current_state[0] = self.quantize(obs.observation.player.vespene)
+
+		hot_squares = np.zeros(4)
+		player_relative = obs.observation.feature_minimap.player_relative  
+		enemy_y, enemy_x = (player_relative == features.PlayerRelative.ENEMY).nonzero()
+		for i in range(0, len(enemy_y)):
+			y = int(math.ceil((enemy_y[i] + 1) / 32))
+			x = int(math.ceil((enemy_x[i] + 1) / 32))
+			
+			hot_squares[((y - 1) * 2) + (x - 1)] = 1
+
+		if not self.base_top_left:
+			hot_squares = hot_squares[::-1]
+
+		for i in range(0, 4):
+			current_state[i + state_elements] = hot_squares[i]
+
+		state_elements += 4
+
+		green_squares = np.zeros(4)        
+		friendly_y, friendly_x = (player_relative == features.PlayerRelative.SELF).nonzero()
+		for i in range(0, len(friendly_y)):
+			y = int(math.ceil((friendly_y[i] + 1) / 32))
+			x = int(math.ceil((friendly_x[i] + 1) / 32))
+			
+			green_squares[((y - 1) * 2) + (x - 1)] = 1
+
+		if not self.base_top_left:
+			green_squares = green_squares[::-1]
+
+		for i in range(0, 4):
+			current_state[i + state_elements] = green_squares[i]
+
+		excluded_actions = []
+
+		if command_center_count >= 1:
+			excluded_actions.append(high_actions.index('BUILD_COMMAND_CENTER'))
+
+		if vespene == 0 or barracks_count == 0:
+			excluded_actions.append(high_actions.index('BUILD_BARRACKS_TECH_LAB'))
+
+		if vespene == 0 or factory_count == 0:
+			excluded_actions.append(high_actions.index('BUILD_FACTORY_TECH_LAB'))
+
+		if factory_count >= 1 or worker_supply == 0:
+			excluded_actions.append(high_actions.index('BUILD_FACTORY'))
+
+		if eng_bay_count >= 1 or worker_supply == 0:
+			excluded_actions.append(high_actions.index('BUILD_ENG_BAY'))
+
+		if refinery_count >= 2 or worker_supply == 0:
+			excluded_actions.append(high_actions.index('BUILD_REFINERY'))
+
+		if supply_depot_count >= 5 or worker_supply == 0:
+			excluded_actions.append(high_actions.index('BUILD_SUPPLY_DEPOT'))
+
+		if supply_depot_count == 0 or barracks_count >= 2 or worker_supply == 0:
+			excluded_actions.append(high_actions.index('BUILD_BARRACKS'))
+
+		if supply_free == 0 or barracks_count == 0 or barracks_tech_lab_count == 0:
+			excluded_actions.append(high_actions.index('TRAIN_BARRACKS_MARINE'))
+			excluded_actions.append(high_actions.index('TRAIN_BARRACKS_REAPER'))
+
+		if supply_free == 0 or barracks_count == 0 or barracks_tech_lab_count == 0 or vespene == 0:
+			excluded_actions.append(high_actions.index('TRAIN_BARRACKS_MARAUDER'))
+
+		if supply_free == 0 or factory_count == 0 or factory_tech_lab_count == 0:
+			excluded_actions.append(high_actions.index('TRAIN_FACTORY_HELLION'))
+
+		if supply_free == 0 or factory_count == 0 or factory_tech_lab_count == 0 or vespene == 0:
+			excluded_actions.append(high_actions.index('TRAIN_FACTORY_TANK'))
+
+		if army_supply == 0:
+			for i, a in enumerate(high_actions):
+				if a.startswith('ATTACK_'):
+					excluded_actions.append(i)
+			
+		return current_state, excluded_actions
+
+	def step(self, obs):
+		super(TableQLearnAgent, self).step(obs)
+
+		# print(obs.observation.score_cumulative.score)
+		if obs.last():
+			reward = obs.reward
+
+			print('\tGame end: Result: %d' % (reward))
+			self.qlearn.learn(str(self.previous_state), self.previous_action, reward, 'terminal')
+			self.qlearn.q_table.to_pickle(self.output_file + '.gz', 'gzip')
+			self.qlearn.q_table.to_csv(self.output_file)
+
+			if reward == -1:
+				result = 'loss'
+			elif reward == 0:
+				result = 'tie'
+			else:
+				result = 'win'
+			
+			self.game_outcomes.append({'game':self.game_count, 'outcome':result, 'score':obs.observation.score_cumulative.score, 'time':datetime.datetime.now()})
+			pd.DataFrame(self.game_outcomes).to_csv(self.output_game_outcome_file)
+			return self.utils.nothing()
+
+		if obs.first():
+			player_y, player_x = (obs.observation.feature_minimap.player_relative == features.PlayerRelative.SELF).nonzero()
+			self.base_top_left = True if player_y.any() and player_y.mean() <= 31 else False
+
+			command_center = self.utils.get_units(obs, units.Terran.CommandCenter)[0]
+			self.command_center_loc = (command_center.x, command_center.y)
+
+		if len(self.queue) == 0:
+			current_state, excluded_actions = self.create_state(obs)
+			# print(current_state.tolist())
+
+			if self.previous_action is not None:
+				self.qlearn.learn(str(self.previous_state), self.previous_action, 0, str(current_state))
+				
+			rl_action = self.qlearn.choose_action(str(current_state), excluded_actions)
+
+			high_action = high_actions[rl_action]
+			new_actions = action_mapping[high_action]
+			self.queue.extend(new_actions)
+
+			self.game_actions.append({'action':high_action, 'time':datetime.datetime.now(), 'state':current_state.tolist(), 'score':obs.observation.score_cumulative.score})
+			pd.DataFrame(self.game_actions).to_csv(self.output_actions_prefix % (self.game_count))
+
+			self.previous_state = current_state
+			self.previous_action = rl_action
+
+		valid_action = self.queue.popleft()
+
+		return self.utils.do(obs, valid_action, self.base_top_left, self.command_center_loc)
